d23ba9508a63a1f481c2be7649054eb5a5117696
devtrium <devtrium325@gmail.com>
2018-05-22 00:45:27 +0800

    use gorrila websocket

--- src/main.go
+++ src/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+    //"fmt"
+    "net/http"
+    "github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
 
 func main() {
-    fmt.Println("Hello World")
+    //fmt.Println("Hello World")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "index.html")
+    })
+
+    http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request) {
+        var conn, _ = upgrader.Upgrade(w, r, nil)
+        go func(conn *websocket.Conn) {
+            for {
+                mType, msg, _ := conn.ReadMessage()
+
+                conn.WriteMessage(mType, msg)
+            }
+        }(conn)
+    })
+
+    http.ListenAndServe(":3000", nil)//handler http.Handler)
 }
